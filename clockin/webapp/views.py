0ebc705119cc7aed7bb8b1bf727ea8ee2f9571ce
RimPeter <hull@localappliancerentals.co.uk>
2024-06-12 07:07:56 +0100

    Add login functionality

--- clockin/webapp/views.py
+++ clockin/webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import CreateUserForm, LoginForm
 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 
 
@@ -18,3 +20,21 @@ def register(request):
             
     context = {'form': form}
     return render(request, 'webapp/register.html', context= context)
+
+# login user
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                auth.login(request, user)
+                return redirect('my-login')
+    context = {'form': form}
+    return render(request, 'webapp/my-login.html', context= context)
+    
